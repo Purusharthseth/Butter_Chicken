0a0477891eae518325880ef8949a5550ef5a8afc
Purusharth Seth <purusharthseth2@gmail.com>
2025-06-05 14:03:02 +0530

    final commit maybe

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import cors from 'cors';
-const app = express();
-app.use(express.json());
-app.use(cors());
 
-//importing routes
+const app = express();
+
+
+app.set('trust proxy', 1);
+
+app.use(express.json());
+
+app.use(cors({
+  origin: '*',          
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type']
+}));
+
+// Importing routes
 import chapterRoutes from "./routes/chapter.routes.js";
-//using routes
+
+// Using routes
 app.use("/api/v1/chapters", chapterRoutes);
-export {app};
+
+export { app };
