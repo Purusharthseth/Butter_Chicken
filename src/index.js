aa3945e2873d1de75a03129360ae45586ebc9fac
Purusharth Seth <purusharthseth2@gmail.com>
2025-06-05 13:35:39 +0530

    trynna fix railway error.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import connectDB from "./config/db.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server is running on port", PORT);
 });
 connectDB()
